0c57451a7217da36dbf097afcb2f37ca70ca78e6
wdb <wu1963688583@163.com>
2022-04-10 01:36:01 +0800

    idea git test3

--- gmall-common/src/main/java/com/atguigu/constants/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/constants/GmallConstants.java
@@ -25,5 +25,7 @@ public class GmallConstants {
     //灵活分析索引别名
     public static final String ES_QUERY_INDEXNAME = "gmall2021_sale_detail-query";
     //public static final String ES_QUERY_INDEXNAME = "gmall2021_sale_detail-query";
-
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
